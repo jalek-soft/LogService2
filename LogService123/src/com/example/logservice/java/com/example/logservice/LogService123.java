fdb507b1cd914f5586068d6b12c6788d21bd1f59
Yuliya Alekseeva <jalek@softserveinc.com>
2021-05-20 18:09:08 +0300

    Java

---
+++ LogService123/src/com/example/logservice/java/com/example/logservice/LogService123.java
@@ -0,0 +1,21 @@
+package com.example.logservice;
+
+import android.app.Application;
+import android.os.ServiceManager;
+import android.util.Log;
+
+
+    public class LogService123 extends Application {
+        private static final String REMOTE_SERVICE_NAME = LogService123.class.getName();
+        private ILogAIDLInterfaceImpl serviceImpl;
+
+        public void onCreate() {
+            super.onCreate();
+            this.serviceImpl = new ILogAIDLInterfaceImpl();
+            ServiceManager.addService(REMOTE_SERVICE_NAME, this.serviceImpl);
+        }
+
+        public void onTerminate() {
+            super.onTerminate();
+        }
+}
